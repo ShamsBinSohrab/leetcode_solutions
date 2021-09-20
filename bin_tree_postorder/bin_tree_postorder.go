bc280ad3fe59ff1659702a5dfa6ba536045453ab
shams <shams.bin_sohrab@welldev.io>
2021-09-20 10:53:52 +0600

    bin_tree_postorder.go

---
+++ bin_tree_postorder/bin_tree_postorder.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+//Problem: https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := &TreeNode{
+		Val:  1,
+		Left: nil,
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val:   3,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: nil,
+		},
+	}
+
+	fmt.Println(postorderTraversal(root))
+}
+
+var postorder func(node *TreeNode)
+
+func postorderTraversal(root *TreeNode) []int {
+	var nodes []int
+	postorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		postorder(node.Left)
+		postorder(node.Right)
+		nodes = append(nodes, node.Val)
+	}
+	postorder(root)
+	return nodes
+}
