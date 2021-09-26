e4fd6bb61f97ab018d3dbe36f1fe4d97fffa8e2a
shams <shams.bin_sohrab@welldev.io>
2021-09-26 13:12:21 +0600

    intersection_of_two_arrays.go

---
+++ intersection_of_two_arrays_2/intersection_of_two_arrays.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+//Problem: https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+func main() {
+	nums1 := []int{4, 9, 5}
+	nums2 := []int{9, 4, 9, 8, 4}
+	fmt.Println(intersect(nums1, nums2))
+}
+
+func intersect(nums1 []int, nums2 []int) []int {
+	i, j, k := 0, 0, 0
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] < nums2[j] {
+			i++
+		} else if nums1[i] > nums2[j] {
+			j++
+		} else {
+			nums1[k] = nums1[i]
+			i++
+			j++
+			k++
+		}
+	}
+	return nums1[:k]
+}
